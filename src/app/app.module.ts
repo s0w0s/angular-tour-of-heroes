069978ccc11de31339a154f57e51f75fb63b29e7
woosung <jangwooseng97@gmail.com>
2021-12-05 20:00:24 +0900

    feat(heros): heros 에디터 추가 및 애플리케이션 동작에 필요한 메타데이터 ngmodule에 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel은 이 패키지가 제공합니다.
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,    
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
