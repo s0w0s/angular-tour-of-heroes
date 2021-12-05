9c5bbceaa44555c56a861e80fff623917a79b336
woosung <jangwooseng97@gmail.com>
2021-12-06 02:00:02 +0900

    feat(hero-service): heror service 주입 및 ngoninit()에서 서비스 호출하기 부분 추가

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +8,20 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes  = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
