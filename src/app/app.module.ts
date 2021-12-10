c1a0146302abec1e5be6929412954afa9a17d734
woosung <jangwooeng97@gmail.ocm>
2021-12-10 18:42:04 +0900

    feat(routing): approutingmodule 생성 및 라우팅 영역 추가 네비게이션 링크 추가 하기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import {AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { MessagesComponent } from './messages/messages.component';
     MessagesComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
